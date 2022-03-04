2052fee2c7b3ee68ca1755827f75296d9cc74a53
MEEP of Faith <meepmatthew@gmail.com>
2022-03-03 19:19:58 -0800

    Sliiiide to the left
    Take it back now yall

--- main/src/unity/ui/DisclaimerDialog.java
+++ main/src/unity/ui/DisclaimerDialog.java
@@ -1,7 +1,9 @@
 package unity.ui;
 
+import arc.math.*;
 import arc.scene.actions.*;
 import arc.scene.ui.*;
+import arc.scene.ui.layout.*;
 import arc.util.*;
 import mindustry.ui.dialogs.*;
 import unity.*;
@@ -14,26 +16,35 @@ public class DisclaimerDialog extends BaseDialog{
         cont.add("@mod.disclaimer.text").width(500f).wrap().pad(4f).get().setAlignment(Align.center, Align.center);
         buttons.defaults().size(200f, 54f).pad(2f);
         setFillParent(false);
-        delayButton("@ok", this::hide, 8, 5);
-        if(shouldSkip()) return;
-        delayButton("@mod.disclaimer.skip", () -> {
-            hide();
-            settings.put("mod.disclaimer.skip", true);
-        }, 17, 5);
-    }
 
-    void delayButton(String text, Runnable listener, float delay, float fadeTime){
-        TextButton b = buttons.button(text, listener).get();
+        TextButton b = buttons.button("@ok", this::hide).get();
+
         if(shouldSkip()) return;
-        //Add a delay to when the ok button can be pressed. Read the damn disclaimer and don't ignore it like you do with every terms and conditions list you see.
+
         b.setDisabled(() -> b.color.a < 1);
+        float moveDst = Scl.scl(101f); //Half button width + Half pad
         b.actions(
-            Actions.alpha(0),
-            Actions.delay(delay),
-            Actions.fadeIn(fadeTime)
+            Actions.alpha(0), Actions.moveBy(moveDst, 0f),
+            Actions.delay(8f),
+            Actions.fadeIn(5f),
+            Actions.delay(4f),
+            Actions.moveBy(-moveDst, 0f, 5f, Interp.smoother)
         );
         b.getStyle().disabledFontColor = b.getStyle().fontColor;
         b.getStyle().disabled = b.getStyle().up;
+
+        TextButton s = buttons.button("@mod.disclaimer.skip", () -> {
+            hide();
+            settings.put("mod.disclaimer.skip", true);
+        }).get();
+        s.setDisabled(() -> s.color.a < 1);
+        s.actions(
+            Actions.alpha(0),
+            Actions.delay(17f),
+            Actions.fadeIn(5f)
+        );
+        s.getStyle().disabledFontColor = s.getStyle().fontColor;
+        s.getStyle().disabled = s.getStyle().up;
     }
 
     boolean shouldSkip(){
